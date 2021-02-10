853119563b5e485c33ec09867fb5e4cf9e4d532e
Artyom <akuzikov@mail.ru>
2021-02-10 17:32:09 +0300

    updated lesson

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,4 +24,4 @@ window.state = store.getState();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// next lesson: https://www.youtube.com/watch?v=MM02LsZqssQ&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=60 14:49
+// next lesson: https://www.youtube.com/watch?v=b3pU3hsJlNk&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=62 : 13:34
