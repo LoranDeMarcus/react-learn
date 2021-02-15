53b2c6d79f6d78aecf6de78f165c7c73142a77dc
Loran <akuzikov@mail.ru>
2021-02-15 20:21:05 +0300

    finished componentDidUpdate

--- my-app/src/components/ProfileStatus/ProfileStatus.jsx
+++ my-app/src/components/ProfileStatus/ProfileStatus.jsx
@@ -19,6 +19,11 @@ class ProfileStatus extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
     }
 
     render() {
